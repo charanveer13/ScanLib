4e99622a43a9b05c5379420e9f482055e2e152ea
charanveer13 <charanveer13@gmail.com>
2019-11-18 19:49:44 +0530

    charan commint

--- scanlibcard/src/main/java/com/example/scanlibcard/ScanNow.java
+++ scanlibcard/src/main/java/com/example/scanlibcard/ScanNow.java
@@ -2,13 +2,20 @@ package com.example.scanlibcard;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.BitmapFactory;
 
+import androidx.fragment.app.Fragment;
+
+import com.fxn.pix.Options;
+import com.fxn.pix.Pix;
+import com.fxn.utility.ImageQuality;
 import com.getbouncer.cardscan.ScanActivity;
 
 public class ScanNow {
 
-    public void scan(Activity activity)
+    public void scan(Fragment activity)
     {
-        ScanActivity.start(activity);
+        Pix.start(activity, Options.init().setRequestCode(100));
     }
 }
